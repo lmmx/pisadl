6ff742b409c2736d327edc81c5493d879f12cdbb
Louis <naivelocus@gmail.com>
2014-07-08 17:04:25 +0100

    Added empty download template

---
+++ getxml1-1.py
@@ -0,0 +1,5 @@
+import urllib2
+import re
+from datetime import datetime #this allows you to drop the leading `datetime.` and makes slightly more concise cf. just import datetime
+with open("interfaces1-1.xml","r+") as outputfile:
+	execfile('download.py')
